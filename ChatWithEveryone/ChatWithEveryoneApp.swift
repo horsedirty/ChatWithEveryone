416b4ce0fd02e0c12ae16052d0e1624acf324d8d
马子昂 <929110464@qq.com>
2026-04-30 22:54:29 +0800

    Initial Commit

---
+++ ChatWithEveryone/ChatWithEveryoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithEveryoneApp.swift
+//  ChatWithEveryone
+//
+//  Created by 马子昂 on 2026/4/30.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithEveryoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatWithEveryone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatWithEveryone
+//
+//  Created by 马子昂 on 2026/4/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
